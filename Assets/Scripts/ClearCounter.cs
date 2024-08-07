29eab4a46bb89a1462f9b18359c48d53ec787ce5
Miraç Tandoğan <tandoganmirac61@gmail.com>
2024-08-07 17:18:58 +0300

    counters preparing

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,8 +8,26 @@ public class ClearCounter : BaseCounter {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-    public override void Interact(Player player)
-    {
-        
+    public override void Interact(Player player){
+        if (!HasKitchenObject()) {
+            //there is no kitchenobject here
+            if (player.HasKitchenObject()){
+                //player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else{
+                // player not carrying anything
+            }
+        }
+        else{
+            //here is a kitchenobject here
+            if (player.HasKitchenObject()){
+                //player is carrying smotething
+            }
+            else{
+                //player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -13,9 +13,14 @@ public class ContainerCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        if (!player.HasKitchenObject())
+        {
+            //player is not carrying anything
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
 
-        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
+
     }
 }
